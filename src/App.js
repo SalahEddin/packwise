5a9532cb2b32f83ef9b3454d612f5f61204ab7ff
salah eddin alshaal <salshaal@gmail.com>
2019-08-31 13:01:21 +0200

    addedanimation to sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Button, Heading, Grommet } from "grommet";
+import { Box, Button, Collapsible, Heading, Grommet } from "grommet";
 import { Notification } from "grommet-icons";
 
 const theme = {
@@ -48,8 +48,9 @@ function App() {
           <Box flex align="center" justify="center">
             app body
           </Box>
-          {showSidebar && (
+          <Collapsible direction="horizontal" open={showSidebar}>
             <Box
+              flex
               width="medium"
               background="light-2"
               elevation="small"
@@ -58,7 +59,7 @@ function App() {
             >
               sidebar
             </Box>
-          )}
+          </Collapsible>
         </Box>
       </Box>
     </Grommet>
