d3dcea674bfa5ccb8203479345c77456b169110d
salah eddin alshaal <salshaal@gmail.com>
2019-08-31 12:59:47 +0200

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
-import React from "react";
-import { Grommet } from "grommet";
+import React, { useState } from "react";
+import { Box, Button, Heading, Grommet } from "grommet";
+import { Notification } from "grommet-icons";
 
 const theme = {
   global: {
-    font: {
-      family: 'Roboto',
-      size: '14px',
-      height: '20px',
+    colors: {
+      brand: "#3ca300"
     },
-  },
+    font: {
+      family: "Roboto",
+      size: "14px",
+      height: "20px"
+    }
+  }
 };
 
+const AppBar = props => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
+
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
-    <Grommet theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar>
+          <Heading level="4" margin="none">
+            My App
+          </Heading>
+          <Button
+            icon={<Notification />}
+            onClick={() => setShowSidebar(!showSidebar)}
+          />
+        </AppBar>
+        <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+          <Box flex align="center" justify="center">
+            app body
+          </Box>
+          {showSidebar && (
+            <Box
+              width="medium"
+              background="light-2"
+              elevation="small"
+              align="center"
+              justify="center"
+            >
+              sidebar
+            </Box>
+          )}
+        </Box>
+      </Box>
     </Grommet>
   );
 }
