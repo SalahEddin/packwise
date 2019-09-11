396ba8a41086b7cd2fcba99e5b8e6816a77856c1
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2019-09-11 22:51:58 +0200

    feat(card): Card atom implementation

---
+++ src/presenters/atoms/card/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  CardContainer,
+  CardHeader,
+  CardBody
+} from 'presenters/atoms/card/index.styles';
+
+function Card(props) {
+  Card.propTypes = {
+    children: PropTypes.node.isRequired,
+    header: PropTypes.string.isRequired,
+    onIconClick: PropTypes.func.isRequired,
+    Icon: PropTypes.node.isRequired,
+    showHeader: PropTypes.bool.isRequired
+  };
+
+  const { Icon, children, header, onIconClick, showHeader } = props;
+
+  return (
+    <CardContainer>
+      {showHeader && (
+        <CardHeader>
+          <span>{header}</span>
+          <Icon onClick={onIconClick} />
+        </CardHeader>
+      )}
+      <CardBody>{children}</CardBody>
+    </CardContainer>
+  );
+}
+
+export default Card;
---
+++ src/presenters/atoms/card/index.styles.js
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+const CardContainer = styled('section')`
+  box-sizing: border-box;
+  box-shadow: 0rem 0.0625rem 0.125rem
+    ${props => props.theme.card.boxShadowColor};
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  max-height: ${props => props.theme.card.maxHeight};
+  margin: ${props => (props.margin ? props.margin : '0')};
+  padding: ${props => (props.padding ? props.padding : '0')};
+`;
+
+const CardHeader = styled('header')`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid ${props => props.theme.card.header.borderBottom};
+  padding: 0.5rem 1rem;
+`;
+
+const CardBody = styled('div')`
+  height: 100%;
+  overflow: auto;
+  margin: ${props => (props.margin ? props.margin : '0')};
+  padding: ${props => (props.padding ? props.padding : '0 1rem')};
+`;
+
+export { CardContainer, CardHeader, CardBody };
