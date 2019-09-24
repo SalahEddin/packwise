eac25d9b14d79dc93c7f9f0355bfe1e99a2b08c5
Salah Eddin Alshaal <salshaal@gmail.com>
2019-09-24 22:23:42 +0200

    update (qa-card): add default values for style

--- src/presenters/atoms/card/index.styles.js
+++ src/presenters/atoms/card/index.styles.js
@@ -3,11 +3,15 @@ import styled from '@emotion/styled';
 const CardContainer = styled('section')`
   box-sizing: border-box;
   box-shadow: 0rem 0.0625rem 0.125rem
-    ${props => props.theme.card.boxShadowColor};
+    ${props =>
+      props.theme.card.boxShadowColor
+        ? props.theme.card.boxShadowColor
+        : 'rgba(0, 0, 0, 0.45)'};
   display: flex;
   flex: 1;
   flex-direction: column;
-  max-height: ${props => props.theme.card.maxHeight};
+  max-height: ${props =>
+    props.theme.card.maxHeight ? props.theme.card.maxHeight : '700px'};
   margin: ${props => (props.margin ? props.margin : '0')};
   padding: ${props => (props.padding ? props.padding : '0')};
 `;
