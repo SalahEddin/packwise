26a33c430202aa06fe43971cc7c02838441e5ca2
Salah Eddin Alshaal <salshaal@gmail.com>
2019-09-24 14:20:37 +0200

    feat (generate-checklist): filter shelters based on conditions

---
+++ src/containers/checklistGenerator/index.js
@@ -0,0 +1,29 @@
+// for now this logic will live on the client-side
+
+export function filterShelters(options, selectedConditions) {
+  let filtered = options.filter(function(entry) {
+    let conditionsSet = entry.apropriate_conditions;
+    for (var i = 0, l = conditionsSet.length; i < l; i++) {
+      let set = conditionsSet[i];
+      if (primitiveArraysEqual(set, selectedConditions)) return true;
+    }
+    return false;
+  });
+  return filtered;
+}
+
+export function primitiveArraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  // Please note that calling sort on an array will modify that array.
+  // you might want to clone your array first.
+  a.sort();
+  b.sort();
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
