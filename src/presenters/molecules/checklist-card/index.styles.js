0c2ee1272a2b8d1b3d585259cd8fa391c70ff857
Salah Eddin Alshaal <salshaal@gmail.com>
2019-09-24 22:20:14 +0200

    refactor (checklist-card): rename component to Checklist

--- src/presenters/molecules/checklist-card/index.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-const CardContainer = styled('div')`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-`;
-export { CardContainer };
