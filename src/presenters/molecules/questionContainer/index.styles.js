9e80c06afae8892a699c47c0ce6e9a820a05c318
Salah Eddin Alshaal <salshaal@gmail.com>
2019-09-25 09:25:53 +0200

    feat (qa-components): add QA container component

---
+++ src/presenters/molecules/questionContainer/index.styles.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const QaSeperator = styled('section')`
+  margin: ${props => (props.margin ? props.margin : '1rem')};
+  padding: ${props => (props.padding ? props.padding : '0')};
+`;
+
+const QaContainer = styled('div')`
+  height: 100%;
+  overflow: auto;
+  margin: ${props => (props.margin ? props.margin : '0')};
+  padding: ${props => (props.padding ? props.padding : '0 1rem')};
+`;
+
+export { QaSeperator, QaContainer };
