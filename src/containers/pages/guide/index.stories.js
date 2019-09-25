4ae9e7d63c59694dfe1fb611ffcf8e132eb27a81
Salah Eddin Alshaal <salshaal@gmail.com>
2019-09-25 15:38:47 +0200

    feat (packing-page) add page that contains the whole process

---
+++ src/containers/pages/guide/index.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import GuidePage from 'containers/pages/guide';
+
+storiesOf('Pages', module).add('quiz', () => <GuidePage />);
