29645af38d5ee555b406f0ca8e44f13c1fd3e79b
Salah Eddin Alshaal <salshaal@gmail.com>
2019-09-25 10:34:57 +0200

    refactor (qa-manager): use value, label keys

--- src/containers/qaManager/index.test.js
+++ src/containers/qaManager/index.test.js
@@ -1,29 +1,29 @@
 import { genWeatherQuestion, genActivities } from './index';
 
 it('non-empty weather question and answers', () => {
-  let { question, options } = genWeatherQuestion();
-  expect(question !== '').toBe(true);
+  let { label, options } = genWeatherQuestion();
+  expect(label !== '').toBe(true);
   expect(options.length > 0).toBe(true);
 });
 
 it('all weather answers have description and values', () => {
   let { options } = genWeatherQuestion();
   options.forEach(o => {
-    expect(o.description && o.description !== '').toBe(true);
+    expect(o.label && o.label !== '').toBe(true);
     expect(o.value.length > 0).toBe(true);
   });
 });
 
 it('non-empty activities question and answers', () => {
-  let { question, options } = genActivities();
-  expect(question !== '').toBe(true);
+  let { label, options } = genActivities();
+  expect(label !== '').toBe(true);
   expect(options.length > 0).toBe(true);
 });
 
 it('all activities have description and values', () => {
   let { options } = genActivities();
   options.forEach(o => {
-    expect(o.activity_ID && o.activity_ID !== '').toBe(true);
-    expect(o.activity_name && o.activity_name !== '').toBe(true);
+    expect(o.value && o.value !== '').toBe(true);
+    expect(o.label && o.label !== '').toBe(true);
   });
 });
