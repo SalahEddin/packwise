c1bdcb8c1acef8f2fa83bc65918fd993730e4121
Salah Eddin Alshaal <salshaal@gmail.com>
2019-09-29 07:09:30 +0200

    style(checklist-card): move PropTypes within component

--- src/presenters/atoms/checklist-group/index.js
+++ src/presenters/atoms/checklist-group/index.js
@@ -6,30 +6,31 @@ import {
   CheckboxesContainer
 } from 'presenters/atoms/checklist-group/index.styles';
 import { Heading } from 'grommet';
-const ChecklistGroup = ({ title, items, onChange }) => (
-  <GroupContainer>
-    <Heading level={2}>{title}</Heading>
-    <CheckboxesContainer>
-      {items.length > 0 ? (
-        items.map(i => (
-          <ChecklistItem
-            key={i.key}
-            text={i.text}
-            isChecked={i.isChecked}
-            onChange={newState => onChange(i.key, newState)}
-          ></ChecklistItem>
-        ))
-      ) : (
-        <li></li>
-      )}
-    </CheckboxesContainer>
-  </GroupContainer>
-);
-
-ChecklistGroup.propTypes = {
-  title: PropTypes.string.isRequired,
-  items: PropTypes.any.isRequired,
-  onChange: PropTypes.func.isRequired
-};
+function ChecklistGroup({ title, items, onChange }) {
+  ChecklistGroup.propTypes = {
+    title: PropTypes.string.isRequired,
+    items: PropTypes.any.isRequired,
+    onChange: PropTypes.func.isRequired
+  };
+  return (
+    <GroupContainer>
+      <Heading level={2}>{title}</Heading>
+      <CheckboxesContainer>
+        {items.length > 0 ? (
+          items.map(i => (
+            <ChecklistItem
+              key={i.key}
+              text={i.text}
+              isChecked={i.isChecked}
+              onChange={newState => onChange(i.key, newState)}
+            ></ChecklistItem>
+          ))
+        ) : (
+          <li></li>
+        )}
+      </CheckboxesContainer>
+    </GroupContainer>
+  );
+}
 
 export default ChecklistGroup;
--- src/presenters/atoms/checklist-item/index.js
+++ src/presenters/atoms/checklist-item/index.js
@@ -3,19 +3,20 @@ import PropTypes from 'prop-types';
 import { StrikeableText } from 'presenters/atoms/checklist-item/index.styles';
 import { CheckBox } from 'grommet';
 
-const ChecklistItem = ({ text, isChecked, onChange }) => (
-  <CheckBox
-    type="checkbox"
-    label={<StrikeableText isStriked={isChecked}>{text}</StrikeableText>}
-    checked={isChecked}
-    onChange={e => onChange(e.target.checked)}
-  />
-);
-
-ChecklistItem.propTypes = {
-  text: PropTypes.string.isRequired,
-  isChecked: PropTypes.bool.isRequired,
-  onChange: PropTypes.func.isRequired
-};
+function ChecklistItem({ text, isChecked, onChange }) {
+  ChecklistItem.propTypes = {
+    text: PropTypes.string.isRequired,
+    isChecked: PropTypes.bool.isRequired,
+    onChange: PropTypes.func.isRequired
+  };
+  return (
+    <CheckBox
+      type="checkbox"
+      label={<StrikeableText isStriked={isChecked}>{text}</StrikeableText>}
+      checked={isChecked}
+      onChange={e => onChange(e.target.checked)}
+    />
+  );
+}
 
 export default ChecklistItem;
