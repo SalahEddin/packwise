2cb6690c4b24093e1bbaa309b40ddc6463d1674f
Salah Eddin Alshaal <salshaal@gmail.com>
2019-09-29 22:36:30 +0200

    feat(checklist): add checklist items and group

---
+++ src/presenters/atoms/checklist/checklistItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StrikeableText } from 'presenters/atoms/checklist/index.styles';
+import { CheckBox } from 'grommet';
+
+function ChecklistItem({ text, isChecked, onChange }) {
+  ChecklistItem.propTypes = {
+    text: PropTypes.string.isRequired,
+    isChecked: PropTypes.bool.isRequired,
+    onChange: PropTypes.func.isRequired
+  };
+  
+  return (
+    <CheckBox
+      type="checkbox"
+      label={<StrikeableText isStriked={isChecked}>{text}</StrikeableText>}
+      checked={isChecked}
+      onChange={e => onChange(e.target.checked)}
+    />
+  );
+}
+
+export default ChecklistItem;
---
+++ src/presenters/atoms/checklist/index.stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, boolean } from '@storybook/addon-knobs/react';
+import ChecklistItem from 'presenters/atoms/checklist/ChecklistItem';
+import ChecklistGroup from 'presenters/atoms/checklist/ChecklistGroup';
+
+let items = [
+  { key: '1', text: 'Potato', isChecked: false },
+  { key: '2', text: 'Shoe', isChecked: true }
+];
+
+const checklist = storiesOf('Atoms|Checklist', module).addDecorator(withKnobs);
+
+checklist.add('Checklist Item', () => {
+  const checked = boolean('Checked item', false);
+
+  return (
+    <ChecklistItem
+      text="Checked state"
+      isChecked={checked}
+      onChange={() => {}}
+    />
+  );
+});
+
+checklist.add('Checklist Group', () => (
+  <ChecklistGroup
+    title={'Dummy Header'}
+    items={items}
+    onChange={() => {}}
+  />
+));
