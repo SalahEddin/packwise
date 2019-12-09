b72910b623b95f0dce57c01ed0ad4954f7115ab9
Salah Eddin Alshaal <salshaal@gmail.com>
2019-12-09 12:29:32 +0100

    chore: deploy to github pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+console.log('what?');
+
+ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
